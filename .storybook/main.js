70af011ccd3276cb7f684276f7cdbb8ba21b6228
MasuqaT <occar@hotmail.co.jp>
2020-10-11 00:39:32 +0900

    Prepare demo environment

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,19 @@
 module.exports = {
-  "stories": [
-    "../**/*.stories.@(mdx|js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-actions",
-    "@storybook/addon-docs"
-  ]
-}
+  stories: ["../**/*.stories.@(mdx|js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-actions", "@storybook/addon-docs"],
+  webpackFinal: async (config) => {
+    const postCssLoader = config.module.rules
+      .find(({ test }) => test.test(".css"))
+      .use.find((ruleSet) =>
+        (typeof ruleSet === "string" ? ruleSet : ruleSet.loader).includes(
+          "postcss-loader"
+        )
+      );
+
+    postCssLoader.options.plugins = [
+      require("postcss-prepend-selector")({ selector: "center " }),
+    ]; // override
+
+    return config;
+  },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,15 @@
+import React from "react";
 import "@system2020s/reset-style/index.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 };
+
+export const decorators = [
+  (Story) => (
+    // To ease specificity issue in demo, using deprecated tag
+    <center style={{ textAlign: "unset" }}>
+      <Story />
+    </center>
+  ),
+];
